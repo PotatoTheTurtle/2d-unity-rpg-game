7890570f3542cfe705d048544d27d00469751ee8
IvanTurbin <ivan.turbin@gmail.com>
2020-04-29 01:13:54 +0300

    Initial commit

---
+++ Assets/Scripts/Player Scripts/HeartManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeartManager : MonoBehaviour {
+
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite halfFullHeart;
+    public Sprite emptyHeart;
+    public FloatValue heartContainers;
+    public FloatValue playerCurrentHealth;
+
+	// Use this for initialization
+	void Start () {
+        UpdateHearts();
+	}
+
+    public void InitHearts()
+    {
+        Debug.Log("INIT HEARTS");
+        for (int i = 0; i < heartContainers.RuntimeValue; i ++)
+        {
+            if (i < hearts.Length)
+            {
+                hearts[i].gameObject.SetActive(true);
+                hearts[i].sprite = fullHeart;
+            }
+        }
+    }
+
+    public void UpdateHearts()
+    {
+        InitHearts();
+        Debug.Log("INIT HEARTS 2");
+       
+        float tempHealth = playerCurrentHealth.RuntimeValue / 2;
+        Debug.Log(tempHealth);
+        for (int i = 0; i < heartContainers.RuntimeValue; i ++)
+        {
+            if(i <= tempHealth-1)
+            {
+                //Full Heart
+                hearts[i].sprite = fullHeart;
+            }else if( i >= tempHealth)
+            {
+                //empty heart
+                hearts[i].sprite = emptyHeart;
+            }else{
+                //half full heart
+                hearts[i].sprite = halfFullHeart;
+            }
+        }
+    }
+
+}
